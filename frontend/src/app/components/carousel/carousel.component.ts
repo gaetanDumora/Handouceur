85f20dfef47fb479732c0d5c1c461ac67ccff9c2
gaetanDumora <dumoragaetan@gmail.com>
2023-03-23 15:06:24 +0100

    simplify a bit carousel

--- frontend/src/app/components/carousel/carousel.component.ts
+++ frontend/src/app/components/carousel/carousel.component.ts
@@ -1,4 +1,12 @@
-import { AfterViewInit, Component, Input } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { Journey } from 'src/app/models/journeys';
 
 interface Slide {
   id: number;
@@ -10,22 +18,12 @@ interface Slide {
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.scss'],
 })
-export class CarouselComponent implements AfterViewInit {
-  @Input() journeyId: number;
-  slides = [
-    { id: 1, src: '../../../assets/photo.jpg' },
-    {
-      id: 2,
-      src: 'https://i-det.unimedias.fr/sites/art-de-vivre/files/styles/large/public/couv-reunion-web-hemis_3029025.jpg?auto=compress%2Cformat&crop=faces%2Cedges&cs=srgb&fit=crop&h=598&w=900',
-    },
-    {
-      id: 3,
-      src: 'https://www.megeve-tourisme.fr/app/uploads/2022/02/ski-domaine-evasion-mont-blanc-megeve.jpg',
-    },
-  ];
-  private HTMLImageElement: HTMLImageElement;
-  private currentSlide: Slide;
-  public isLoading = true;
+export class CarouselComponent implements AfterViewInit, OnInit {
+  @Input() journey: Journey;
+  @ViewChild('img') img: ElementRef;
+  private slides: Slide[];
+  public currentSlide: Slide;
+  public isImageLoaded: boolean;
 
   constructor() {}
 
@@ -33,27 +31,39 @@ export class CarouselComponent implements AfterViewInit {
     const previousId = this.currentSlide.id - 1;
     const previousSlide = this.slides.find(({ id }) => id === previousId);
     this.currentSlide = previousSlide || this.slides[this.slides.length - 1];
-    return this.loadImage(this.currentSlide.src);
+    return this.loadImage();
   }
 
   onNextClick() {
     const nextId = this.currentSlide.id + 1;
     const nextSlide = this.slides.find(({ id }) => id === nextId);
     this.currentSlide = nextSlide || this.slides[0];
-    return this.loadImage(this.currentSlide.src);
+    return this.loadImage();
   }
 
-  private loadImage(url: string) {
-    this.isLoading = true;
-    this.HTMLImageElement.onload = () => (this.isLoading = false);
-    this.HTMLImageElement.src = url;
+  private loadImage() {
+    this.img.nativeElement.onload = () => {
+      this.isImageLoaded = true;
+    };
+  }
+
+  ngOnInit() {
+    this.isImageLoaded = false;
+    this.slides = [
+      { id: 1, src: this.journey.imageUrl! },
+      {
+        id: 2,
+        src: 'https://i-det.unimedias.fr/sites/art-de-vivre/files/styles/large/public/couv-reunion-web-hemis_3029025.jpg?auto=compress%2Cformat&crop=faces%2Cedges&cs=srgb&fit=crop&h=598&w=900',
+      },
+      {
+        id: 3,
+        src: 'https://www.megeve-tourisme.fr/app/uploads/2022/02/ski-domaine-evasion-mont-blanc-megeve.jpg',
+      },
+    ];
+    this.currentSlide = this.slides[0];
   }
 
   ngAfterViewInit() {
-    this.HTMLImageElement = document.getElementById(
-      `image-${this.journeyId}`
-    ) as HTMLImageElement;
-    this.currentSlide = this.slides[0];
-    return this.loadImage(this.currentSlide.src);
+    this.loadImage();
   }
 }
