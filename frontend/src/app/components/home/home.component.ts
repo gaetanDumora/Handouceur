4856480d8b20d77e5e1f35478e8b213c973e2395
gaetanDumora <dumoragaetan@gmail.com>
2023-03-26 16:53:23 +0200

    clean

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Journey } from 'src/app/models/journeys';
 import { Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
