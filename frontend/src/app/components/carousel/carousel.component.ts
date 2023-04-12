b74dcde3728a1dab15c1d2cabc39c2bf40a352da
gaetanDumora <dumoragaetan@gmail.com>
2023-04-12 22:30:18 +0200

    add default image

--- frontend/src/app/components/carousel/carousel.component.ts
+++ frontend/src/app/components/carousel/carousel.component.ts
@@ -6,11 +6,14 @@ import {
   OnInit,
   ViewChild,
 } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { BehaviorSubject } from 'rxjs';
 import { Journey } from 'src/app/models/journeys';
+import { environment } from 'src/environment/environment';
 
 interface Slide {
   id: number;
-  src: string;
+  key: string;
 }
 
 @Component({
@@ -21,47 +24,41 @@ interface Slide {
 export class CarouselComponent implements AfterViewInit, OnInit {
   @Input() journey: Journey;
   @ViewChild('img') img: ElementRef;
+  private baseURL = `${environment.apiUrl}/journey/image/`;
+  private defaultSlide: Slide = { id: 0, key: this.baseURL + 'empty.jpg' };
   private slides: Slide[];
   public currentSlide: Slide;
-  public isImageLoaded: boolean;
+  public isImageLoaded = new BehaviorSubject(false);
 
-  constructor() {}
+  constructor(private store: Store) {}
 
   onPreviousClick() {
     const prevId = this.currentSlide.id - 1;
     const prevSlide = this.slides.find(({ id }) => id === prevId);
-    return this.loadSlideImage(
-      prevSlide || this.slides[this.slides.length - 1]
-    );
+    const slideToDisplay =
+      prevSlide || this.slides[this.slides.length - 1] || this.defaultSlide;
+    return this.loadSlideImage(slideToDisplay);
   }
 
   onNextClick() {
     const nextId = this.currentSlide.id + 1;
     const nextSlide = this.slides.find(({ id }) => id === nextId);
-    return this.loadSlideImage(nextSlide || this.slides[0]);
+    const slideToDisplay = nextSlide || this.slides[0] || this.defaultSlide;
+    return this.loadSlideImage(slideToDisplay);
   }
 
   private loadSlideImage(slide: Slide) {
-    this.img.nativeElement.onload = () => {
-      this.isImageLoaded = true;
-    };
+    this.img.nativeElement.onload = () => this.isImageLoaded.next(true);
+    this.img.nativeElement.onerror = () => this.isImageLoaded.next(false);
     this.currentSlide = slide;
   }
 
   ngOnInit() {
-    this.isImageLoaded = false;
-    this.slides = [
-      { id: 1, src: this.journey.imageUrl! },
-      {
-        id: 2,
-        src: 'https://i-det.unimedias.fr/sites/art-de-vivre/files/styles/large/public/couv-reunion-web-hemis_3029025.jpg?auto=compress%2Cformat&crop=faces%2Cedges&cs=srgb&fit=crop&h=598&w=900',
-      },
-      {
-        id: 3,
-        src: 'https://www.megeve-tourisme.fr/app/uploads/2022/02/ski-domaine-evasion-mont-blanc-megeve.jpg',
-      },
-    ];
-    this.currentSlide = this.slides[0];
+    this.slides = this.journey.images!.map((key, id) => ({
+      id,
+      key: this.baseURL + key,
+    }));
+    this.currentSlide = this.slides[0] || this.defaultSlide;
   }
 
   ngAfterViewInit() {
