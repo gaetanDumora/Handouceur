121fc7c52b5d2bd97118fa4bce5c488d7aaa1708
gaetanDumora <dumoragaetan@gmail.com>
2023-05-27 15:35:44 +0200

    refactor changeTheme

--- frontend/src/app/layout/nav/nav.component.ts
+++ frontend/src/app/layout/nav/nav.component.ts
@@ -1,7 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { BehaviorSubject, Observable, map } from 'rxjs';
+import { Component } from '@angular/core';
 
-import { User } from 'src/app/models/user';
 import { Store } from '@ngrx/store';
 import { isDarkTheme } from 'src/app/store/root/root.selectors';
 import { ROOT_ACTIONS } from 'src/app/store/root/root.actions';
@@ -12,23 +10,16 @@ import { getUser } from 'src/app/store/user/user.selectors';
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss'],
 })
-export class NavComponent implements OnInit {
-  isDarkTheme: BehaviorSubject<boolean>;
-  user: Observable<User | null>;
-
+export class NavComponent {
+  isDarkTheme = this.store.select(isDarkTheme);
+  user = this.store.select(getUser);
+  hasChanged = false;
   constructor(private store: Store) {}
 
-  ngOnInit() {
-    this.store
-      .select(isDarkTheme)
-      .pipe(map((theme) => (this.isDarkTheme = new BehaviorSubject(theme))))
-      .subscribe();
-    this.user = this.store.select(getUser);
-  }
-
   changeTheme() {
-    const isDarkTheme = !this.isDarkTheme.value;
-    this.isDarkTheme.next(isDarkTheme);
-    this.store.dispatch(ROOT_ACTIONS.setDarkTheme({ isDarkTheme }));
+    this.store.dispatch(
+      ROOT_ACTIONS.setDarkTheme({ isDarkTheme: this.hasChanged })
+    );
+    this.hasChanged = !this.hasChanged;
   }
 }
