4bd2da96e663261e5c114bf9be4cb16e5b6dd2c6
gaetanDumora <dumoragaetan@gmail.com>
2023-05-29 16:52:02 +0200

    use content layout in the app template rather to call it in routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule, isDevMode } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ContentLayoutComponent } from './features/home/content-layout/content-layout.component';
+import { ContentLayoutComponent } from './features/layout/content-layout/content-layout.component';
 import { DialogLogin } from './features/auth/login/login.component';
 import { JourneyListComponent } from './features/journey/journey-list/journey-list.component';
 import { AdminGuard, AuthGuard } from './shared/authentication/auth.guard';
@@ -8,48 +8,42 @@ import { DialogRegister } from './features/auth/register/register.component';
 
 const routes: Routes = [
   {
-    path: '',
-    component: ContentLayoutComponent,
+    path: 'admin',
+    loadComponent: () =>
+      import('./features/user/admin/admin.component').then(
+        (mod) => mod.AdminComponent
+      ),
+    canActivate: [AdminGuard],
+  },
+  {
+    path: 'profile',
+    loadComponent: () =>
+      import('./features/user/profile/profile.component').then(
+        (mod) => mod.ProfileComponent
+      ),
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'home',
+    component: JourneyListComponent,
     children: [
       {
-        path: 'admin',
-        loadComponent: () =>
-          import('./features/user/admin/admin.component').then(
-            (mod) => mod.AdminComponent
-          ),
-        canActivate: [AdminGuard],
+        path: 'register',
+        component: DialogRegister,
       },
       {
-        path: 'profile',
-        loadComponent: () =>
-          import('./features/user/profile/profile.component').then(
-            (mod) => mod.ProfileComponent
-          ),
-        canActivate: [AuthGuard],
-      },
-      {
-        path: 'home',
-        component: JourneyListComponent,
-        children: [
-          {
-            path: 'register',
-            component: DialogRegister,
-          },
-          {
-            path: 'login',
-            component: DialogLogin,
-          },
-        ],
-      },
-      {
-        path: 'home/edit/:id',
-        loadComponent: () =>
-          import('./features/journey/journey-edit/journey-edit.component').then(
-            (mod) => mod.JourneyEditComponent
-          ),
-        canActivate: [AdminGuard],
+        path: 'login',
+        component: DialogLogin,
       },
     ],
+  },
+  {
+    path: 'home/edit/:id',
+    loadComponent: () =>
+      import('./features/journey/journey-edit/journey-edit.component').then(
+        (mod) => mod.JourneyEditComponent
+      ),
+    canActivate: [AdminGuard],
   },
 
   // { path: '**', component: PageNotFoundComponent }, TO DO
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { JOURNEY_ACTIONS } from './store/journey/journey.actions';
 import { RouterModule } from '@angular/router';
+import { ContentLayoutComponent } from './features/layout/content-layout/content-layout.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, ContentLayoutComponent],
 })
 export class AppComponent implements OnInit {
   constructor(private store: Store) {}
