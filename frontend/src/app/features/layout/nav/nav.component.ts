aed8a2d2f3ee50d859ea0522624a1fb9e7fb215b
gaetanDumora <dumoragaetan@gmail.com>
2023-06-17 22:25:29 +0200

    show user avatar in the nav

--- frontend/src/app/features/layout/nav/nav.component.ts
+++ frontend/src/app/features/layout/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Store } from '@ngrx/store';
 import { isDarkTheme } from 'src/app/store/root/root.selectors';
@@ -10,6 +10,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { environment } from 'src/environment/environment';
 
 @Component({
   selector: 'app-nav',
@@ -25,12 +27,19 @@ import { RouterModule } from '@angular/router';
     RouterModule,
   ],
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
   isDarkTheme = this.store.select(isDarkTheme);
   user = this.store.select(getUser);
   hasChanged = false;
+  profileImg: Observable<string>;
   constructor(private store: Store) {}
-
+  ngOnInit(): void {
+    this.user.subscribe((attr) => {
+      if (attr?.avatar) {
+        this.profileImg = of(`${environment.apiUrl}/storage/${attr.avatar}`);
+      }
+    });
+  }
   changeTheme() {
     this.store.dispatch(
       ROOT_ACTIONS.setDarkTheme({ isDarkTheme: this.hasChanged })
