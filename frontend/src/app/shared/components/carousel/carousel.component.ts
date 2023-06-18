c12b7fc8ae54615b455385c870ebed772df14ad7
gaetanDumora <dumoragaetan@gmail.com>
2023-06-18 17:19:58 +0200

    handle empty journey images directly in template

--- frontend/src/app/shared/components/carousel/carousel.component.ts
+++ frontend/src/app/shared/components/carousel/carousel.component.ts
@@ -12,6 +12,7 @@ import { environment } from 'src/environment/environment';
 import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
+import { StorageFolders } from 'src/app/types/storage';
 
 interface Slide {
   id: number;
@@ -28,12 +29,9 @@ interface Slide {
 export class CarouselComponent implements AfterViewInit, OnInit {
   @Input() journey: Journey;
   @ViewChild('img') img: ElementRef;
-  private baseURL = `${environment.apiUrl}/journey/image/`;
-  private defaultSlide: Slide = {
-    id: 0,
-    key: this.baseURL + 'empty.jpg',
-  };
-  private slides: Slide[];
+  private baseURL = `${environment.apiUrl}/storage/`;
+  public fallbackImageUrl = `${this.baseURL}${StorageFolders.journey}empty.jpg`;
+  public slides: Slide[];
   public currentSlide: Slide;
   public isImageLoaded = new BehaviorSubject(false);
 
@@ -42,15 +40,14 @@ export class CarouselComponent implements AfterViewInit, OnInit {
   onPreviousClick() {
     const prevId = this.currentSlide.id - 1;
     const prevSlide = this.slides.find(({ id }) => id === prevId);
-    const slideToDisplay =
-      prevSlide || this.slides[this.slides.length - 1] || this.defaultSlide;
+    const slideToDisplay = prevSlide || this.slides[this.slides.length - 1];
     return this.loadSlideImage(slideToDisplay);
   }
 
   onNextClick() {
     const nextId = this.currentSlide.id + 1;
     const nextSlide = this.slides.find(({ id }) => id === nextId);
-    const slideToDisplay = nextSlide || this.slides[0] || this.defaultSlide;
+    const slideToDisplay = nextSlide || this.slides[0];
     return this.loadSlideImage(slideToDisplay);
   }
 
@@ -65,14 +62,15 @@ export class CarouselComponent implements AfterViewInit, OnInit {
         id,
         key: this.baseURL + key,
       }));
+      this.currentSlide = this.slides[0];
     }
-
-    this.currentSlide = this.slides ? this.slides[0] : this.defaultSlide;
   }
 
   ngAfterViewInit() {
-    this.img.nativeElement.onload = () => this.isImageLoaded.next(true);
-    this.img.nativeElement.onerror = () => this.isImageLoaded.next(false);
-    this.loadSlideImage(this.currentSlide);
+    if (this.img) {
+      this.img.nativeElement.onload = () => this.isImageLoaded.next(true);
+      this.img.nativeElement.onerror = () => this.isImageLoaded.next(false);
+      this.loadSlideImage(this.currentSlide);
+    }
   }
 }
